2e6d3a7562f78bf0ab5738511207ef0f6203daad
MohaSalem <64562801+MohaSalem@users.noreply.github.com>
2020-09-13 18:07:09 +0300

    Add files via upload

---
+++ Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StackOverFlowPostMimic
+{
+    public class Post
+    {
+        public string Title { get; set; } 
+        public string Description { get; set; }
+        public DateTime PostDate { get; private set; }
+        public int Votes { get; private set; }
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.PostDate = DateTime.Now;
+            this.Votes = 0;
+        }
+
+        public void UpVote()
+        {
+            Votes++;
+        }
+
+        public void DownVote()
+        {
+            Votes--;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackOverFlowPostMimic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("C#", "Xamarin");
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.UpVote();
+            post.DownVote();
+            Console.WriteLine("The post title is {0}, the post description is {1}, the post publishing date " +
+                              "and time are {2} and the post votes are {3}", post.Title, post.Description, post.PostDate, post.Votes);
+        }
+    }
+}
